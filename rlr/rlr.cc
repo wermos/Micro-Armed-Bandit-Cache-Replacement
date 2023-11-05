4b3040c8271320b399f3f39327770be77034ea6b
divyanshtiwari237 <200020049@iitb.ac.in>
2023-11-05 12:19:22 +0530

    some changes to rlr

--- rlr/rlr.cc
+++ rlr/rlr.cc
@@ -15,43 +15,37 @@ enum Type
 class SetRD
 {
     private:
-    std::vector<int> entries;
+    int entries;
     int accum;
+    int rd;
     
     public:
     SetRD():
-    accum(0)
+    accum(0), rd(2), entries(0)
     {
     }
 
     void addEntry(int age)
     {
-        if(entries.size() <= 32)
-        {
-            entries.push_back(age);
-            accum += age;
-        }
-        else
-        {
-            entries.push_back(age);
-            accum += age - entries[0];
-            entries.erase(entries.begin());
-        }
 
+       
+        accum += age;
+        //std::cout<<"accum:"<<accum<<std::endl;
+        entries++;
+
+        if(entries == 32)
+        {
+            rd = std::max((2* accum)/entries,2);
+            //std::cout<<"rd"<<rd<<"accum" <<accum<<std::endl;
+            entries = 0;
+            accum = 0;
+        }
     }
 
     int returnRD()
     {
-        if(entries.size() < 32)
-        {
-            // how do we initialize RD for a set with less that 32 entries?
-            // feel like we should allows all the cache line till then to have high age priority, so return a high RD
-            return INT_MAX;
-        }
-        else
-        {
-            return accum/entries.size(); // just the int part is fine I think
-        }
+         
+       return rd;
     }
 
 };
@@ -92,19 +86,21 @@ class Status
 
     void countAge() // counter of age
     {
-        internalCounter++;
-        if(internalCounter%8 == 0)
-        {
-            internalCounter = 0;
-            ageCounter++;
-        }
+       
+        // internalCounter++;
+        // if(internalCounter%8 == 0)
+        // {
+        //     internalCounter = 0;
+        //     ageCounter++;
+        // }
+        ageCounter++;
     }
 
     int returnPriority(int reuseDistance) // calculates all the priorities and outputs final priority
     {
         ageP = ageCounter < reuseDistance ? High : Low;
         typeHitP = typeHit == 1 ? High: Low;
-        typeAccessP = typeAccess != 1? High: Low;
+        typeAccessP = typeAccess == 1? High: Low;
 
         int priority = 8*ageP + typeHitP + typeAccessP;
         return priority;
@@ -115,7 +111,16 @@ class Status
         return ageCounter;
     }
 
-    void reset(int access) // same object can be used to track the next cache line that enters this WAY of the set
+    int returnTypeAccessP()
+    {
+        return typeAccessP;
+    }
+    int returnTypeHitP()
+    {
+        return typeHitP;
+    }
+
+    void reset(int access) // same object can be used to track the next  line that enters this WAY of the set
     {
         ageCounter = 0;
         typeHit = 0;
@@ -139,26 +144,24 @@ void CACHE::initialize_replacement()
 
 uint32_t CACHE::find_victim(uint32_t triggering_cpu, uint64_t instr_id, uint32_t set, const BLOCK* current_set, uint64_t ip, uint64_t full_addr, uint32_t type)
 {
-    // is this called even if the set has empty slots? assuming yes
-    for(int i = 0; i < NUM_WAY; i++) // if we have an empty slot, use that(don't know if we need to do this)
-    {
-        if(feature_entries[this][set*NUM_WAY + i].occu == 0)
-        {
-            return i;
-        }
-    }
+    // is this called even if the set has empty slots? assuming no
+   
 
     std::vector<std::vector<int>>priorityList;
     // store priority, recency and way number of all slots to determine which way to evict
     for(int i = 0; i < NUM_WAY; i++)
     {
-        if(feature_entries[this][set*NUM_WAY + i].occu !=0)
-        {
-            priorityList.push_back({feature_entries[this][set*NUM_WAY + i].returnPriority(rd_entries[this][set].returnRD()), feature_entries[this][set*NUM_WAY + i].returnAge(), i});
-        }
+        
+        priorityList.push_back({feature_entries[this][set*NUM_WAY + i].returnPriority(rd_entries[this][0].returnRD()), feature_entries[this][set*NUM_WAY + i].returnAge(), i});
+        
     }
 
     sort(priorityList.begin(), priorityList.end());
+    // std::cout<<"the access type of evicted line is:"<<feature_entries[this][set*NUM_WAY + priorityList[priorityList.size() - 1][2]].returnTypeAccessP()<<std::endl;
+    // std::cout<<"the hit type of evicted line is:"<<feature_entries[this][set*NUM_WAY + priorityList[priorityList.size() - 1][2]].returnTypeHitP()<<std::endl;
+    // std::cout<<"priority of evicted: "<<priorityList[priorityList.size() - 1][0]<<std::endl;
+    // std::cout<<"way of evicted: "<<priorityList[priorityList.size() - 1][2]<<std::endl;
+
     return priorityList[0][2]; // return way index of least priority slot
 
 
@@ -168,35 +171,42 @@ void CACHE::update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint
                                      uint8_t hit)
 {
     // what is up with all the write back bullshit happening in every other policy inside `update_replacement_state`
-
-    // update ageCounters for all the WAYS
+    if (hit && type == WRITEBACK)
+    return;
+    
     for(int i = 0; i < NUM_WAY; i++)
     {
-        if(i == way && hit)
-        {
-            rd_entries[this][set].addEntry(feature_entries[this][set*NUM_WAY + i].returnAge()); // since its a demand hit, use the age of this cache line to alter re-use distance of the set
-            feature_entries[this][set*NUM_WAY + i].setAgeCounter(0); // reset the age
-            continue;
-        }
+        if(i == way)
+        continue;
 
-        if(feature_entries[this][set*NUM_WAY + i].occu == 1) // only increment the age of those entries of the set which are occupied(initially we might have empty slots)
+       // if(feature_entries[this][set*NUM_WAY + i].occu == 1) // only increment the age of those entries of the set which are occupied(initially we might have empty slots)
         feature_entries[this][set*NUM_WAY + i].countAge(); // increment age for all other WAYS in this set
     }
 
-    //update hit status and typeAccess
+   
+    
+    //update hit status and typeAccess and age
     if(hit)
     {
+        
+        rd_entries[this][0].addEntry(feature_entries[this][set*NUM_WAY + way].returnAge()); // since its a demand hit, use the age of this cache line to alter re-use distance of the set
+        feature_entries[this][set*NUM_WAY + way].setAgeCounter(0); // reset the age
+        
+
         feature_entries[this][set*NUM_WAY + way].setTypeHit(1); // I know we need to set this only once per cache line, but what's the harm?
         if(type == PREFETCH)
         {
             feature_entries[this][set*NUM_WAY + way].setTypeAccess(0); // check and update access type
-
+        }
+        else
+        {
+            feature_entries[this][set*NUM_WAY + way].setTypeAccess(1);
         }
     }
     else
     {
         // a miss is filled in this way, we reset the status
-        int access = type == PREFETCH? 0 : 1;
+        int access = type == PREFETCH? 1 : 0;
         feature_entries[this][set*NUM_WAY + way].reset(access);
     }
 }
