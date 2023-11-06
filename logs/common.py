7012262f4ce694491a788f03caebaedb5167686b
wermos <63574588+wermos@users.noreply.github.com>
2023-11-06 21:48:46 +0530

    Moved the log file generation scripts to `logs` and refactored the full log generation code.

--- logs/common.py
+++ logs/common.py
@@ -1,7 +1,9 @@
-import re
 import os
+import re
 import subprocess
-from tqdm.contrib.concurrent import process_map 
+from multiprocessing import Pool
+from functools import partial
+from tqdm import tqdm
 
 def get_trace_names():
     with open("memory_intensive_traces.txt", "r") as f:
@@ -11,18 +13,23 @@ def get_trace_names():
     return trace_names
 
 # Function to process each trace file
-def process_trace(trace_filename):
-    command = f"./scripts/run_champsim.sh ./traces/{trace_filename}.trace.xz > ./logs/ship/{trace_filename}.txt"
+def process_trace(trace_filename, output_dir):
+    command = f"./scripts/run_champsim.sh ./traces/{trace_filename}.trace.xz > ./logs/{output_dir}/{trace_filename}.txt"
 
     # Execute the command using subprocess
     subprocess.run(command, shell=True)
 
-if __name__ == "__main__":
+def generate_trace_logs(output_dir):
     # List of all the memory intensive trace files
     trace_filenames = get_trace_names()
 
     # Number of parallel processes to run
     num_processes = len(os.sched_getaffinity(0))  # Using all available CPU cores
+    
+    process_trace_private = partial(process_trace, output_dir=output_dir)
 
     # Use tqdm to display a progress bar
-    r = process_map(process_trace, trace_filenames, max_workers=num_processes)
+    with Pool(processes=num_processes) as pool:
+        with tqdm(total=len(trace_filenames)) as pbar:
+            for _ in pool.imap_unordered(process_trace_private, trace_filenames):
+                pbar.update()
---
+++ logs/generate-ship-data.py
@@ -0,0 +1,4 @@
+from common import generate_trace_logs
+
+if __name__ == "__main__":
+    generate_trace_logs("ship")
