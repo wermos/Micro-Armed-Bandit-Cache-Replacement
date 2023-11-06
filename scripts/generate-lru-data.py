6efe302feafdf7118ee736601561204ec32e128b
wermos <63574588+wermos@users.noreply.github.com>
2023-11-06 11:44:56 +0530

    Fixed the script and modified the text file to make it more easily usable.

--- scripts/generate-lru-data.py
+++ scripts/generate-lru-data.py
@@ -1,22 +1,25 @@
+import re
 import os
 import subprocess
 from multiprocessing import Pool
 
-def get_traces():
+def get_trace_names():
     with open("memory_intensive_traces.txt", "r") as f:
         traces = f.read().splitlines()
-    return traces
+    
+    trace_names = [re.sub(".trace.xz", "", string) for string in traces]
+    return trace_names
 
 # Function to process each trace file
-def process_trace(trace_file):
-    command = f"./scripts/run_champsim {trace_file} > ./logs/lru/{trace_file}.txt"
+def process_trace(trace_filename):
+    command = f"./scripts/run_champsim ./traces/{trace_file}.trace.xz > ./logs/lru/{trace_filename}.txt"
     
     # Execute the command using subprocess
     subprocess.run(command, shell=True)
 
 if __name__ == "__main__":
-    # List all the memory intensive trace files
-    trace_files = get_traces()
+    # List of all the memory intensive trace files
+    trace_filenames = get_trace_names()
 
     # Number of parallel processes to run
     num_processes = len(os.sched_getaffinity(0))  # Using all available CPU cores
