3edb9cb020e4a8be86c87382f373243f19b8ab6a
wermos <63574588+wermos@users.noreply.github.com>
2023-11-23 20:12:10 +0530

    Finished implementing multi-armed bandits.

--- bandit/multi_armed_bandit.hpp
+++ bandit/multi_armed_bandit.hpp
@@ -1,5 +1,22 @@
 #pragma once
 
-class MultiArmedBandit {
+#include <cstddef>
 
+#include "policy.hpp"
+
+class MultiArmedBandit {
+    public:
+        MultiArmedBandit(std::size_t numArms, Policy* policy) : N{numArms}, policy{policy} {}
+
+        std::size_t nextArm() {
+            return policy->selectNextArm();
+        }
+
+        void updateRewards(std::size_t arm, double reward) {
+            policy->updateState(arm, reward);
+        }
+    private:
+        // The number of arms
+        std::size_t N;
+        Policy* policy;
 };
