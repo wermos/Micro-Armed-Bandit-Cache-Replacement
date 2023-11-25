c9894a8fc9ffbde50a00ae29f076618c4575146d
wermos <63574588+wermos@users.noreply.github.com>
2023-11-25 23:53:45 +0530

    Improved code formatting.

--- micro-armed-bandit/orchestrator.hpp
+++ micro-armed-bandit/orchestrator.hpp
@@ -1,21 +1,19 @@
 #pragma once
-#include "../bandit/policy.hpp"
+
+#include "bandit/multi_armed_bandit.hpp"
+#include "bandit/policy.hpp"
+
 #include "replacement-policy/replacement_policy.hpp"
 #include "replacement-policy/drrip.hpp"
 #include "replacement-policy/lru.hpp"
 #include "replacement-policy/ship.hpp"
 #include "replacement-policy/srrip.hpp"
-#include "../bandit/multi_armed_bandit.hpp"
 
+int curr_policy = 1;  // global variable
 
-int curr_policy = 1; // global variable
-
-
-class Orchestrator
-{
-    public:
-    Orchestrator(std::size_t N): N(N), c(c), gamma(gamma)
-    {
+class Orchestrator {
+   public:
+    Orchestrator(std::size_t N) : N(N), c(c), gamma(gamma) {
         DUCB* policy = new DUCB(N, c, gamma);
         bandit = MultiArmedBandit(N, policy);
 
@@ -23,39 +21,30 @@ class Orchestrator
         replacementPolicy.push_back(new SRRIP());
         replacementPolicy.push_back(new SHIP());
         replacementPolicy.push_back(new LRU());
-
     }
 
-    void initialize_replacement()
-    {
-        for(int i = 0; i < replacementPolicy.size(); i++)
-        {
+    void initialize_replacement() {
+        for (int i = 0; i < replacementPolicy.size(); i++) {
             replacementPolicy[i]->initialize_replacement();
         }
     }
 
-    uint32_t find_victim(uint32_t triggering_cpu, uint64_t instr_id, uint32_t set, const BLOCK* current_set, uint64_t ip, uint64_t full_addr, uint32_t type)
-    {
-        replacementPolicy[curr_policy]->find_victum( triggering_cpu,  instr_id,  set, current_set,  ip,  full_addr,  type);
-
+    uint32_t find_victim(uint32_t triggering_cpu, uint64_t instr_id, uint32_t set, const BLOCK* current_set,
+                         uint64_t ip, uint64_t full_addr, uint32_t type) {
+        replacementPolicy[curr_policy]->find_victum(triggering_cpu, instr_id, set, current_set, ip, full_addr, type);
     }
 
-    void update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip, uint64_t victim_addr, uint32_t type, uint8_t hit)
-    {
-        for(int i = 0; i < replacementPolicy.size(); i++)
-        {
-            replacementPolicy[i]-> update_replacement (triggering_cpu,  set,  way,  full_addr,  ip,  victim_addr,  type, hit);
+    void update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip,
+                                  uint64_t victim_addr, uint32_t type, uint8_t hit) {
+        for (int i = 0; i < replacementPolicy.size(); i++) {
+            replacementPolicy[i]->update_replacement(triggering_cpu, set, way, full_addr, ip, victim_addr, type, hit);
         }
-
     }
 
-    private:
+   private:
     std::size_t N;
     double c;
     double gamma;
     MultiArmedBandit bandit;
     std::vector<ReplacementPolicy*> replacementPolicy;
-
-
-
 };
