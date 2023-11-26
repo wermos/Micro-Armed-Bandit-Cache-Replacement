1b72e5678bed6d953eb1f9554969e5db42cadbdb
wermos <63574588+wermos@users.noreply.github.com>
2023-11-26 21:29:10 +0530

    Made SRRIP into a class.

--- micro-armed-bandit/replacement-policy/srrip.hpp
+++ micro-armed-bandit/replacement-policy/srrip.hpp
@@ -1,45 +1,50 @@
 #include <cassert>
-
-#include "cache.h"
 #include <unordered_map>
 
-namespace
-{
+#include "cache.h"
+
+#include "replacement-policy.hpp"
+
+namespace {
 constexpr int maxRRPV = 3;
-std::unordered_map<CACHE*, std::vector<int>> rrpv_values;
-} // namespace
+}  // anonymous namespace
 
-// initialize replacement state
-void CACHE::initialize_replacement() { ::rrpv_values[this] = std::vector<int>(NUM_SET * NUM_WAY, ::maxRRPV); }
+class SRRIP : ReplacementPolicy {
+   public:
+    SRRIP() = default;
 
-// find replacement victim
-uint32_t CACHE::find_victim(uint32_t triggering_cpu, uint64_t instr_id, uint32_t set, const BLOCK* current_set, uint64_t ip, uint64_t full_addr, uint32_t type)
-{
-  // look for the maxRRPV line
-  auto begin = std::next(std::begin(::rrpv_values[this]), set * NUM_WAY);
-  auto end = std::next(begin, NUM_WAY);
-  auto victim = std::find(begin, end, ::maxRRPV); // hijack the lru field
-  while (victim == end) {
-    for (auto it = begin; it != end; ++it)
-      ++(*it);
+    virtual void initialize(CACHE* cache_block) override {
+        rrpv_values[cache_block] = std::vector<int>(cache_block->NUM_SET * cache_block->NUM_WAY, ::maxRRPV);
+    }
 
-    victim = std::find(begin, end, ::maxRRPV);
-  }
+    virtual void updateState(CACHE* cache_block, std::uint64_t current_cycle, std::uint32_t triggering_cpu,
+                             std::uint32_t set, std::uint32_t way, std::uint64_t full_addr, std::uint64_t ip,
+                             std::uint64_t victim_addr, std::uint32_t type, std::uint8_t hit) override {
+        if (hit)
+            rrpv_values[cache_block][set * cache_block->begin_phase + way] = 0;
+        else
+            rrpv_values[cache_block][set * cache_block->begin_phase + way] = ::maxRRPV - 1;
+    }
 
-  assert(begin <= victim);
-  assert(victim < end);
-  return static_cast<uint32_t>(std::distance(begin, victim)); // cast protected by assertions
-}
+    virtual std::uint32_t findVictim(CACHE* cache_block, std::uint64_t current_cycle, std::uint32_t triggering_cpu,
+                                     std::uint64_t instr_id, std::uint32_t set, const CACHE::BLOCK* current_set,
+                                     std::uint64_t ip, std::uint64_t full_addr, std::uint32_t type) override {
+        // look for the maxRRPV line
+        auto begin = std::next(std::begin(rrpv_values[cache_block]), set * cache_block->NUM_WAY);
+        auto end = std::next(begin, cache_block->NUM_WAY);
+        auto victim = std::find(begin, end, ::maxRRPV);  // hijack the lru field
+        while (victim == end) {
+            for (auto it = begin; it != end; ++it)
+                ++(*it);
 
-// called on every cache hit and cache fill
-void CACHE::update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip, uint64_t victim_addr, uint32_t type,
-                                     uint8_t hit)
-{
-  if (hit)
-    ::rrpv_values[this][set * NUM_WAY + way] = 0;
-  else
-    ::rrpv_values[this][set * NUM_WAY + way] = ::maxRRPV - 1;
-}
+            victim = std::find(begin, end, ::maxRRPV);
+        }
 
-// use this function to print out your own stats at the end of simulation
-void CACHE::replacement_final_stats() {}
+        assert(begin <= victim);
+        assert(victim < end);
+        return static_cast<std::uint32_t>(std::distance(begin, victim));  // cast protected by assertions
+    }
+
+   private:
+    std::unordered_map<CACHE*, std::vector<int>> rrpv_values;
+};
