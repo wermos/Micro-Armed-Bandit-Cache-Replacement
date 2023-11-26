b2be35a5f7f9192909b734e5023dac29a3835ee1
wermos <63574588+wermos@users.noreply.github.com>
2023-11-26 21:37:30 +0530

    Modified the class to use more standard library constructs.

--- micro-armed-bandit/orchestrator.hpp
+++ micro-armed-bandit/orchestrator.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cstdint>
+#include <memory>
+
 #include "bandit/multi_armed_bandit.hpp"
 #include "bandit/policy.hpp"
 
@@ -9,35 +12,35 @@
 #include "replacement-policy/ship.hpp"
 #include "replacement-policy/srrip.hpp"
 
-int curr_policy = 1;  // global variable
-
 class Orchestrator {
    public:
-    Orchestrator(std::size_t N) : N(N), c(c), gamma(gamma) {
+    Orchestrator(std::size_t N) : N{N}, c{c}, gamma{gamma}, curr_policy{0} {
         DUCB* policy = new DUCB(N, c, gamma);
         bandit = MultiArmedBandit(N, policy);
 
-        replacementPolicy.push_back(new DRRIP());
-        replacementPolicy.push_back(new SRRIP());
-        replacementPolicy.push_back(new SHIP());
-        replacementPolicy.push_back(new LRU());
+        replacementPolicy.push_back(std::make_unique<LRU>());
+        replacementPolicy.push_back(std::make_unique<DRRIP>());
+        replacementPolicy.push_back(std::make_unique<SHIP>()));
+        replacementPolicy.push_back(std::make_unique<SRRIP>());
     }
 
     void initialize_replacement() {
-        for (int i = 0; i < replacementPolicy.size(); i++) {
-            replacementPolicy[i]->initialize_replacement();
+        for (auto policy : replacementPolicy) {
+            policy->initialize();
         }
     }
 
-    uint32_t find_victim(uint32_t triggering_cpu, uint64_t instr_id, uint32_t set, const BLOCK* current_set,
-                         uint64_t ip, uint64_t full_addr, uint32_t type) {
-        replacementPolicy[curr_policy]->find_victum(triggering_cpu, instr_id, set, current_set, ip, full_addr, type);
+    std::uint32_t findVictim(CACHE* cache_block, std::uint64_t current_cycle, std::uint32_t triggering_cpu,
+                             std::uint64_t instr_id, std::uint32_t set, const CACHE::BLOCK* current_set,
+                             std::uint64_t ip, std::uint64_t full_addr, std::uint32_t type) override {
+        replacementPolicy[curr_policy]->findVictim(cache_block, current_cycle, triggering_cpu, instr_id, set, current_set, ip, full_addr, type);
     }
 
-    void update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip,
-                                  uint64_t victim_addr, uint32_t type, uint8_t hit) {
-        for (int i = 0; i < replacementPolicy.size(); i++) {
-            replacementPolicy[i]->update_replacement(triggering_cpu, set, way, full_addr, ip, victim_addr, type, hit);
+    void updateState(CACHE* cache_block, std::uint64_t current_cycle, std::uint32_t triggering_cpu,
+                     std::uint32_t set, std::uint32_t way, std::uint64_t full_addr, std::uint64_t ip,
+                     std::uint64_t victim_addr, std::uint32_t type, std::uint8_t hit)
+        for (auto policy : replacementPolicy) {
+            policy->updateState(cache_block, current_cycle, triggering_cpu, set, way, full_addr, ip, victim_addr, type, hit);
         }
     }
 
@@ -46,5 +49,6 @@ class Orchestrator {
     double c;
     double gamma;
     MultiArmedBandit bandit;
-    std::vector<ReplacementPolicy*> replacementPolicy;
+    std::vector<std::unique_ptr<ReplacementPolicy>> replacementPolicy;
+    curr_policy;
 };
