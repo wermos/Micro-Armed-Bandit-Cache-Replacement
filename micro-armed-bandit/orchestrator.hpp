53d7be009b570eb0abc1e628f5aa47826de6f072
wermos <63574588+wermos@users.noreply.github.com>
2023-11-26 23:18:12 +0530

    Fixed up the implementation of the Orchestrator class.

--- micro-armed-bandit/orchestrator.hpp
+++ micro-armed-bandit/orchestrator.hpp
@@ -4,6 +4,9 @@
 #include <memory>
 #include <vector>
 
+#include "cache.h"
+#include "ooo_cpu.h"
+
 #include "bandit/multi_armed_bandit.hpp"
 #include "bandit/policy.hpp"
 
@@ -14,25 +17,25 @@
 
 class Orchestrator {
    public:
-    Orchestrator(std::size_t N) : N{N}, c{0.01}, gamma{0.975}, currentPolicy{0}, nextUpdateCycle{0} {
+    Orchestrator(std::size_t n) : N{n}, c{0.01}, gamma{0.975}, currentPolicy{0}, nextUpdateCycle{0} {
         DUCB* policy = new DUCB(N, c, gamma);
         bandit = MultiArmedBandit(N, policy);
 
-        replacementPolicy.push_back(std::make_unique<LRU>());
-        replacementPolicy.push_back(std::make_unique<DRRIP>());
-        replacementPolicy.push_back(std::make_unique<SHIP>()));
-        replacementPolicy.push_back(std::make_unique<SRRIP>());
+        replacementPolicy.push_back(std::make_shared<LRU>());
+        replacementPolicy.push_back(std::make_shared<DRRIP>());
+        replacementPolicy.push_back(std::make_shared<SHIP>());
+        replacementPolicy.push_back(std::make_shared<SRRIP>());
     }
 
     void initialize(CACHE* cache_block) {
-        for (auto policy : replacementPolicy) {
+        for (const auto& policy : replacementPolicy) {
             policy->initialize(cache_block);
         }
     }
 
-    void updatePolicyState(CACHE* cache_block, std::uint64_t current_cycle, std::uint32_t triggering_cpu,
+    void updateState(CACHE* cache_block, std::uint64_t current_cycle, std::uint32_t triggering_cpu,
                      std::uint32_t set, std::uint32_t way, std::uint64_t full_addr, std::uint64_t ip,
-                     std::uint64_t victim_addr, std::uint32_t type, std::uint8_t hit)
+                     std::uint64_t victim_addr, std::uint32_t type, std::uint8_t hit) {
         if (current_cycle >= nextUpdateCycle) {
             // update all the MAB stuff
             bandit.updateRewards(currentPolicy, ::current_ipc);
@@ -41,12 +44,12 @@ class Orchestrator {
             nextUpdateCycle += MAB_IPC_UPDATE_FREQUENCY;
         }
 
-        updatePolicyState(cache_block, current_cycle, triggering_cpu, instr_id, set, current_set, ip, full_addr, type);
+        updatePolicyState(cache_block, current_cycle, triggering_cpu, set, way, full_addr, ip, victim_addr, type, hit);
     }
 
     std::uint32_t findVictim(CACHE* cache_block, std::uint64_t current_cycle, std::uint32_t triggering_cpu,
                              std::uint64_t instr_id, std::uint32_t set, const CACHE::BLOCK* current_set,
-                             std::uint64_t ip, std::uint64_t full_addr, std::uint32_t type) override {
+                             std::uint64_t ip, std::uint64_t full_addr, std::uint32_t type) {
         replacementPolicy[currentPolicy]->findVictim(cache_block, current_cycle, triggering_cpu, instr_id, set, current_set, ip, full_addr, type);
     }
 
@@ -54,8 +57,8 @@ class Orchestrator {
     // updates the internal state of all the policies
     void updatePolicyState(CACHE* cache_block, std::uint64_t current_cycle, std::uint32_t triggering_cpu,
                      std::uint32_t set, std::uint32_t way, std::uint64_t full_addr, std::uint64_t ip,
-                     std::uint64_t victim_addr, std::uint32_t type, std::uint8_t hit)
-        for (auto policy : replacementPolicy) {
+                     std::uint64_t victim_addr, std::uint32_t type, std::uint8_t hit) {
+        for (const auto& policy : replacementPolicy) {
             policy->updateState(cache_block, current_cycle, triggering_cpu, set, way, full_addr, ip, victim_addr, type, hit);
         }
     }
@@ -64,7 +67,7 @@ class Orchestrator {
     double c;
     double gamma;
     MultiArmedBandit bandit;
-    std::vector<std::unique_ptr<ReplacementPolicy>> replacementPolicy;
+    std::vector<std::shared_ptr<ReplacementPolicy>> replacementPolicy;
     std::size_t currentPolicy;
     // the next cycle during which the MAB is to be updated
     std::uint64_t nextUpdateCycle;
