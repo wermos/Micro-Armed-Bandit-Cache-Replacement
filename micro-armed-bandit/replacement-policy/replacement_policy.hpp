8e43be3ff0b85eeafcf5f089f0a259f46b96e270
wermos <63574588+wermos@users.noreply.github.com>
2023-11-26 18:36:59 +0530

    Updated the interface's function definitions.

--- micro-armed-bandit/replacement-policy/replacement_policy.hpp
+++ micro-armed-bandit/replacement-policy/replacement_policy.hpp
@@ -5,10 +5,11 @@
 #include "cache.h"
 
 class ReplacementPolicy {
-    virtual void initialize() = 0;
-    virtual void update(std::uint32_t triggering_cpu, std::uint32_t set, std::uint32_t way, std::uint64_t full_addr,
-                        std::uint64_t ip, std::uint64_t victim_addr, std::uint32_t type, std::uint8_t hit) = 0;
-    virtual void find_victim(std::uint32_t triggering_cpu, std::uint64_t instr_id, std::uint32_t set,
-                             const BLOCK* current_set, std::uint64_t ip, std::uint64_t full_addr,
-                             std::uint32_t type) = 0;
+    virtual void initialize(CACHE*) = 0;
+    virtual void updateState(CACHE* cache_block, std::uint64_t current_cycle, std::uint32_t triggering_cpu,
+                             std::uint32_t set, std::uint32_t way, std::uint64_t full_addr, std::uint64_t ip,
+                             std::uint64_t victim_addr, std::uint32_t type, std::uint8_t hit) = 0;
+    virtual std::uint32_t findVictim(CACHE* cache_block, std::uint64_t current_cycle, std::uint32_t triggering_cpu,
+                                     std::uint64_t instr_id, std::uint32_t set, const CACHE::BLOCK* current_set,
+                                     std::uint64_t ip, std::uint64_t full_addr, std::uint32_t type) = 0;
 };
