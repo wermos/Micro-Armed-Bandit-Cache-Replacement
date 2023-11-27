832dfa29256d8083037fbfc18d11efcf1977ef7a
wermos <63574588+wermos@users.noreply.github.com>
2023-11-27 11:43:42 +0530

    Made the script calculate the overall speedup for all the policies.

--- scripts/overall-ipc-speedup.py
+++ scripts/overall-ipc-speedup.py
@@ -10,9 +10,11 @@ drrip_data = {}
 ship_data = {}
 hawkeye_data = {}
 rlr_data = {}
+mab_data = {}
+srrip_data = {}
 
 # Iterate over subdirectories
-for subdir in ['lru', 'drrip', 'ship', 'hawkeye', 'rlr']:
+for subdir in ['lru', 'drrip', 'ship', 'hawkeye', 'rlr', 'micro-armed-bandit', 'srrip']:
     subdir_path = os.path.join(main_directory, subdir)
     files = os.listdir(subdir_path)
 
@@ -36,6 +38,10 @@ for subdir in ['lru', 'drrip', 'ship', 'hawkeye', 'rlr']:
                     ship_data[file[:-4]] = float(ipc)
                 elif subdir == 'rlr':
                     rlr_data[file[:-4]] = float(ipc)
+                elif subdir == 'micro-armed-bandit':
+                    mab_data[file[:-4]] = float(ipc)
+                elif subdir == 'srrip':
+                    srrip_data[file[:-4]] = float(ipc)
             else:
                 data = f.read().splitlines()[26]
 
@@ -47,16 +53,29 @@ lru_values = list(lru_data.values())
 drrip_values = list(drrip_data.values())
 ship_values = list(ship_data.values())
 hawkeye_values = list(hawkeye_data.values())
+srrip_values = list(srrip_data.values())
 rlr_values = list(rlr_data.values())
+mab_values = list(mab_data.values())
 
-# product = [1] * 4 # one for each of the non-LRU policies
-product = 1
+product = [1] * 6 # one for each of the non-LRU policies
+# product = 1
 
 # for j, _ in enumerate(['drrip', 'ship', 'hawkeye', 'rlr']):
 #     for i in range(len(lru_values)):
 #         product[j] *= rlr_values[i] / lru_values[i]
 
 for i in range(len(lru_values)):
-    product *= rlr_values[i] / lru_values[i]
+    product[0] *= drrip_values[i] / lru_values[i]
+    product[1] *= ship_values[i] / lru_values[i]
+    product[2] *= hawkeye_values[i] / lru_values[i]
+    product[3] *= srrip_values[i] / lru_values[i]
+    product[4] *= rlr_values[i] / lru_values[i]
+    product[5] *= mab_values[i] / lru_values[i]
 
-print(f"Overall IPC Speedup over LRU = {product ** (1 / len(lru_values))}")
+print(f"Overall IPC Speedup over LRU:")
+print(f"DRRIP: {product[0] ** (1 / len(lru_values))}")
+print(f"SHIP: {product[1] ** (1 / len(lru_values))}")
+print(f"Hawkeye: {product[2] ** (1 / len(lru_values))}")
+print(f"SRRIP: {product[3] ** (1 / len(lru_values))}")
+print(f"RLR : {product[4] ** (1 / len(lru_values))}")
+print(f"Micro-armed bandit: {product[5] ** (1 / len(lru_values))}")
